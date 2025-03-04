1d132b1a8a53876d71ad86acb25dafb09dc2b365
Joedele <moboladeoladele2008@gmail.com>
2025-03-04 11:37:07 +0100

    Add files via upload

---
+++ day 1_100 days - Replit.py
@@ -0,0 +1,11 @@
+print("Mobolade")
+print("March 4, 2025")
+
+print("""
+I am signing up for Replit's 100 days of Python challenge!🥳 🥳
+I will make sure to spend some time everyday coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE, so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!😂😂
+I am feeling 😊😊
+""")
+
+print("You can follow my progress at replit.com/@moboladeoladele. See you in 100 days✌️")
