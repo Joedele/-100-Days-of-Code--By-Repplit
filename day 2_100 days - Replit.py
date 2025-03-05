090795955c1ff9c217f9e586ef5a7364b8e6aa56
Joedele <moboladeoladele2008@gmail.com>
2025-03-05 11:15:19 +0100

    Add files via upload

---
+++ day 2_100 days - Replit.py
@@ -0,0 +1,22 @@
+userName = input("What is your name? ")
+favFood = input("What's your favorite food? ")
+favMusic = input("What type of music do you love? ")
+userLocation = input("Where do you live? ")
+
+print()
+
+print("Wow")
+print(userName)
+print("you like")
+print(favFood)
+print("too! I thought I was the only one.")
+
+print()
+
+print("Do you know that I was actually planning on coming over to")
+print(userLocation)
+print("next month?")
+print("Maybe we could meet up. And who knows we might just listen to some of that good")
+print(favMusic)
+print("music you were talking about.")
+print("Till then, take care. I hope to see you soon.")
