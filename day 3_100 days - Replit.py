ab436077cf7468d2f2ccee6a348eb630d3b3e737
Joedele <moboladeoladele2008@gmail.com>
2025-03-06 07:47:11 +0100

    Add files via upload

---
+++ day 3_100 days - Replit.py
@@ -0,0 +1,19 @@
+print()
+#To leave a bit of space at the top of the code.
+
+print("My Wacky Recipe Maker")
+print()
+aFood = input("Name your favorite food: ")
+aPlant = input("Name a common plant: ")
+aCookingMethod = input("Name a method of cooking: ")
+aSynonym = input("What word best describes burnt food: ")
+diyItem = input("Name a DIY item: ")
+
+print()
+
+print("YOUR CUSTOM MENU")
+print(aCookingMethod, aFood,"with",aSynonym,aPlant,"on a bed of",diyItem)
+print("What!! That doesn't make sense😅😅🤣")
+
+print()
+#Prints a space below the program.
