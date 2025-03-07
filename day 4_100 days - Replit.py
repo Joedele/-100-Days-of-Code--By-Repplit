713a087a474cd6de7d5560f5aeae9e5914e28a85
Joedele <moboladeoladele2008@gmail.com>
2025-03-07 09:25:28 +0100

    Add files via upload

---
+++ day 4_100 days - Replit.py
@@ -0,0 +1,37 @@
+print()
+
+print("\033[33m","Welcome to the story simulator 🥳 🥳. I'll ask you a set of questions whose answers will be used to create an epic tale.")
+print("Our character today is going to be a strange little star from outer space. ")
+print("""\033[0mWhat made it strange?
+      What eventually happened to it?
+      Let's find out.""")
+print("\033[32m","Sit back, relax and enjoy this wonderful tale","\033[0m")
+print()
+starName= input("\033[30mName your star: \033[0m")
+get_age = input("\033[30mWhen was it formed? \033[0m")
+superPower = input("\033[30mGive it a superpower? \033[0m")
+print()
+print() 
+
+#The draft of the story was made by ChatGPT, but the final edit and the coding were done by me.
+
+print("\033[32m","The Tale of a Star 🌟", "\033[0m")
+print()
+#There's nothing here. The space is just for aesthetics.
+print("Long ago, in a universe not so far away, there was a special star named", "\033[33m",starName,"\033[0m",". Born in the year","\033[33m",get_age,"\033[0m",", this star glowed much brighter than all the others. But one thing in particular made it unique—it had","\033[33m",superPower,"\033[0m.")
+print()
+#There's nothing here. The space is just for aesthetics.
+print("With this gift,","\033[33m",starName,"\033[0m","amazed the planets and moons around it. So much so that even the Sun and Moon were afraid of it. Why? Because it wasn’t an ordinary star.")
+print()
+#There's nothing here. The space is just for aesthetics.
+print("Although, it had great power, respect, and fame,","\033[33m",starName,"\033[0m"," sometimes felt lonely. Occasionally, it would look into the vast beyond, wondering how it came to be, who made it, and what its purpose was.")
+print()
+#There's nothing here. The space is just for aesthetics.
+print("Sometimes when it missed it's mother, it would send out a large beam of light hoping that she could see him, wherever she was. But this act made the other stars around him more terrified of him, so they all isolated themselves from him, adding to his grief and sorrow.")
+print()
+#There's nothing here. The space is just for aesthetics.
+print("After years and years of sorrow and grief, the day came when the star would shine no more. It looked into the sky one last time, and emitted so large a light, that the Universe sprang into existence. And that was the end of this great star.")
+
+print()
+
+#I’m not a believer or fan of the big bang theory, I just needed a sad and exiting end for my story, so don’t look at me in a terrible light. 
