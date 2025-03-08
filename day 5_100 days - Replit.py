52ccac40d7beb981ccf9962f3ebac2d7cb1b9eac
Joedele <moboladeoladele2008@gmail.com>
2025-03-08 10:21:11 +0100

    Add files via upload

---
+++ day 5_100 days - Replit.py
@@ -0,0 +1,59 @@
+print()
+
+print("\033[33m","What am I?","\033[0m")
+print()
+
+
+#This line is just to add to the aesthetics of the program
+Riddle1 = input("What has to be broken before you can use it? ").lower()
+
+if Riddle1 == "egg" or Riddle1 == "an egg":
+  print("\033[32m","  ✅ You're right 🥚🍳","\033[0m")
+else:
+  print('\033[91m',"  ❌ It's an egg 🥚","\033[0m")
+print()
+
+
+#This line is just to add to the aesthetics of the program
+Riddle2 = input("I’m tall when I’m young, and I’m short when I’m old. What am I? ").lower()
+
+if Riddle2 == "candle" or Riddle2 == "a candle":
+  print("\033[32m","  ✅ You're right 🕯️","\033[0m")
+else:
+  print('\033[91m',"  ❌ It's a candle 🕯️","\033[0m")
+print()
+
+#This line is just to add to the aesthetics of the program
+Riddle3 = input("Which month(s) of the year has 28 days? ").lower()
+
+if Riddle3 == "all of them" or Riddle3 == "all 12 of them" or Riddle3 == "12 months":
+  print("\033[32m","  ✅ You're right 🌏","\033[0m")
+else:
+  print('\033[91m',"  ❌ It's all of them 🌏","\033[0m")
+print()
+
+#This line is just to add to the aesthetics of the program
+Riddle4 = input("What is full of holes but still holds water? ").lower()
+
+if Riddle4 == "sponge" or Riddle4 == "a sponge":
+  print("\033[32m","  ✅ You're right 🧽💦","\033[0m")
+else:
+  print('\033[91m',"  ❌ It's a sponge 🧽💦","\033[0m")
+print()
+
+#This line is just to add to the aesthetics of the program
+Riddle5 = input("What question can you never answer yes to? ").lower()
+
+if Riddle5 == "are you asleep yet?" or Riddle5 == "are you asleep?":
+  print("\033[32m","  ✅ You're right 😴","\033[0m")
+else:
+  print('\033[91m',"  ❌ It's are you asleep yet 😴","\033[0m")
+
+#This line is just to add to the aesthetics of the program
+print()
+print()
+print("\033[33m","That's the end of my program. Thanks so much for trying it out. If you'd like to check out more of such riddles, you can visit this url:","\033[30m", "https://parade.com/947956/parade/riddles/","\033[0m")
+print("\033[94m","That's all for now , bye!","\033[0m")
+
+print()
+print()
